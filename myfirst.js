200877b095d9a551d746b38da10b77e53e88a7b1
Ofallbrains <ofallbrains>
2025-05-02 16:17:30 +0200

    Writing to a file using fs module of node js

--- myfirst.js
+++ myfirst.js
@@ -10,15 +10,15 @@
 
 //Reading files with fs.readfile method
 //Ex1.
-// const fs = require('fs')
-// fs.readFile('files/example.txt', 'utf8', (err, data) => {
-//     if (err) throw err
-//     console.log(data)
-// })
+const fs = require('fs')
+fs.readFile('files/example.txt', 'utf8', (err, data) => {
+    if (err) throw err
+    console.log(data)
+})
 
 //Ex2.
-const fs = require('fs')
-fs.readFile('files/config.json', 'utf-8', (err, data) => {
+const readfs = require('fs')
+readfs.readFile('files/config.json', 'utf-8', (err, data) => {
     if (err) {
         console.error("Error loading:", err);
         return;
@@ -29,3 +29,22 @@ fs.readFile('files/config.json', 'utf-8', (err, data) => {
 })
 
 //writing to a file using fs.writefile
+//Ex1.
+const writefs = require('fs')
+const log = 'User logged in at:' + new Date()
+writefs.writeFile('files/app.log', log, (err) => {
+    if (err) {
+        console.log("Error writing file:", err)
+        return
+    }
+    console.log("Log was successfully written");
+    
+})
+
+//Ex2.
+const writefs2 = require('fs')
+writefs2.writeFile("files/file.txt", "This is learning nodejs", (err) => {
+    if (err) throw err;
+    console.log("Error was found while writing");
+    
+})
