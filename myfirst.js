7a849abc6b6cb968cc1b90ad68e01bb0ab5ee1a5
Ofallbrains <ofallbrains>
2025-05-02 15:48:58 +0200

    Reading files using fs module of nodejs

--- myfirst.js
+++ myfirst.js
@@ -9,8 +9,23 @@
 // })
 
 //Reading files with fs.readfile method
+//Ex1.
+// const fs = require('fs')
+// fs.readFile('files/example.txt', 'utf8', (err, data) => {
+//     if (err) throw err
+//     console.log(data)
+// })
+
+//Ex2.
 const fs = require('fs')
-fs.readFile('files/example.txt', 'utf8', (err, data) => {
-    if (err) throw err
-    console.log(data)
+fs.readFile('files/config.json', 'utf-8', (err, data) => {
+    if (err) {
+        console.error("Error loading:", err);
+        return;
+        
+    } else {
+        console.log("File contents:", JSON.parse(data))
+    }
 })
+
+//writing to a file using fs.writefile
